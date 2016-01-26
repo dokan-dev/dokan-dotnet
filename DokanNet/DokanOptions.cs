6465f1137ac73ad5b5afb35c036a2c19016b1a09
Adrien JUND <liryna.stark@gmail.com>
2016-01-25 23:07:14 -0800

    1.0.0: Add new dokan mount options and change DllImport dokan.dll to dokan-1.dll

--- DokanNet/DokanOptions.cs
+++ DokanNet/DokanOptions.cs
@@ -8,8 +8,10 @@ namespace DokanNet
         DebugMode = 1, // ouput debug message
         StderrOutput = 2, // ouput debug message to stderr
         AltStream = 4, // use alternate stream
+        WriteProtection = 8, // mount drive as write-protected.
         NetworkDrive = 16, // use network drive, you need to install Dokan network provider.
         RemovableDrive = 32, // use removable drive
+        MountManager = 64, // use mount manager
         FixedDrive = 0,
     }
 }
--- DokanNet/Native/NativeMethods.cs
+++ DokanNet/Native/NativeMethods.cs
@@ -5,29 +5,29 @@ namespace DokanNet.Native
 {
     internal static class NativeMethods
     {
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         public static extern int DokanMain(ref DOKAN_OPTIONS options, ref DOKAN_OPERATIONS operations);
 
-        [DllImport("dokan.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [DllImport("dokan-1.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
         public static extern int DokanUnmount(char driveLetter);
 
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         public static extern uint DokanVersion();
 
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         public static extern uint DokanDriverVersion();
 
-        [DllImport("dokan.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [DllImport("dokan-1.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
         public static extern int DokanRemoveMountPoint([MarshalAs(UnmanagedType.LPWStr)] string mountPoint);
 
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool DokanResetTimeout(uint timeout, DokanFileInfo rawFileInfo);
 
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         public static extern IntPtr DokanOpenRequestorToken(DokanFileInfo rawFileInfo);
 
-        [DllImport("dokan.dll", ExactSpelling = true)]
+        [DllImport("dokan-1.dll", ExactSpelling = true)]
         public static extern void DokanMapKernelToUserCreateFileFlags(uint FileAttributes,
                                                                         uint CreateOptions,
                                                                         uint CreateDisposition,
@@ -35,7 +35,7 @@ namespace DokanNet.Native
                                                                         ref int outCreationDisposition);
 
         /*
-        [DllImport("dokan.dll", CharSet = CharSet.Unicode)]
+        [DllImport("dokan-1.dll", CharSet = CharSet.Unicode)]
         public static extern bool DokanIsNameInExpression([MarshalAs(UnmanagedType.LPWStr)] string expression,
                                                           // matching pattern
                                                           [MarshalAs(UnmanagedType.LPWStr)] string name, // file name
