b2a9038d7ba091596e199d5d24bad6e7a7626614
Bessonov Dmitri <degree56@ya.ru>
2016-02-18 11:12:54 +0500

    Add support for custom library path

--- DokanNet/Native/NativeMethods.cs
+++ DokanNet/Native/NativeMethods.cs
@@ -3,42 +3,49 @@ using System.Runtime.InteropServices;
 
 namespace DokanNet.Native
 {
-    internal static class NativeMethods
+    internal delegate int DokanMain(ref DOKAN_OPTIONS options, ref DOKAN_OPERATIONS operations);
+    internal delegate int DokanUnmount(char driveLetter);
+    internal delegate uint DokanVersion();
+    internal delegate uint DokanDriverVersion();
+    internal delegate int DokanRemoveMountPoint([MarshalAs(UnmanagedType.LPWStr)] string mountPoint);
+    [return:MarshalAs(UnmanagedType.Bool)]
+    internal delegate bool DokanResetTimeout(uint timeout, DokanFileInfo rawFileInfo);
+    internal delegate IntPtr DokanOpenRequestorToken(DokanFileInfo rawFileInfo);
+    internal delegate void DokanMapKernelToUserCreateFileFlags(uint fileAttributes,
+        uint createOptions,
+        uint createDisposition,
+        ref int outFileAttributesAndFlags,
+        ref int outCreationDisposition);
+
+    internal class NativeMethods
     {
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        public static extern int DokanMain(ref DOKAN_OPTIONS options, ref DOKAN_OPERATIONS operations);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string library);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern IntPtr GetProcAddress(IntPtr library, string name);
+        
+        public NativeMethods(string dll = "dokan1.dll")
+        {
+            var library = LoadLibrary(dll);
+            if (library == IntPtr.Zero) throw new DllNotFoundException($"Couldn't load {dll}");
+            foreach (var field in GetType().GetFields())
+            {
+                var addr = GetProcAddress(library, field.Name);
+                if (addr == IntPtr.Zero)
+                    throw new DllNotFoundException($"Couldn't load {field.Name} in {dll}");
+                field.SetValue(this, Marshal.GetDelegateForFunctionPointer(addr, field.FieldType));
+            }
+        }
 
-        [DllImport("dokan1.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
-        public static extern int DokanUnmount(char driveLetter);
-
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        public static extern uint DokanVersion();
-
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        public static extern uint DokanDriverVersion();
-
-        [DllImport("dokan1.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
-        public static extern int DokanRemoveMountPoint([MarshalAs(UnmanagedType.LPWStr)] string mountPoint);
-
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool DokanResetTimeout(uint timeout, DokanFileInfo rawFileInfo);
-
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        public static extern IntPtr DokanOpenRequestorToken(DokanFileInfo rawFileInfo);
-
-        [DllImport("dokan1.dll", ExactSpelling = true)]
-        public static extern void DokanMapKernelToUserCreateFileFlags(uint FileAttributes,
-                                                                        uint CreateOptions,
-                                                                        uint CreateDisposition,
-                                                                        ref int outFileAttributesAndFlags,
-                                                                        ref int outCreationDisposition);
-
-        /*
-        [DllImport("dokan1.dll", CharSet = CharSet.Unicode)]
-        public static extern bool DokanIsNameInExpression([MarshalAs(UnmanagedType.LPWStr)] string expression,
-                                                          // matching pattern
-                                                          [MarshalAs(UnmanagedType.LPWStr)] string name, // file name
-                                                          [MarshalAs(UnmanagedType.Bool)] bool ignoreCase);*/
+#pragma warning disable 649
+        public readonly DokanMain DokanMain;
+        public readonly DokanUnmount DokanUnmount;
+        public readonly DokanVersion DokanVersion;
+        public readonly DokanDriverVersion DokanDriverVersion;
+        public readonly DokanRemoveMountPoint DokanRemoveMountPoint;
+        public readonly DokanResetTimeout DokanResetTimeout;
+        public readonly DokanOpenRequestorToken DokanOpenRequestorToken;
+        public readonly DokanMapKernelToUserCreateFileFlags DokanMapKernelToUserCreateFileFlags;
+#pragma warning restore 649
     }
 }
