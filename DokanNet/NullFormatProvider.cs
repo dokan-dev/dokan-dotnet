ce8d6ed90e0cbb8f2d9279d3e78e04c5b8431cef
magol <magnusosterlund78@gmail.com>
2016-08-12 16:39:08 +0100

    Add comments

--- DokanNet/NullFormatProvider.cs
+++ DokanNet/NullFormatProvider.cs
@@ -7,21 +7,38 @@ namespace DokanNet
     /// </summary>
     public class FormatProviders : IFormatProvider, ICustomFormatter
     {
+        /// <summary>
+        /// A Singleton instanse of this class.
+        /// </summary>
         public static readonly FormatProviders DefaultFormatProvider = new FormatProviders();
+
+        /// <summary>
+        /// A constant string that representet what to use if the formated object is <c>null</c>.
+        /// </summary>
         public static readonly string NullStringRepresentation = "<null>";
 
+        /// <summary>
+        /// Format a <see cref="FormattableString"/> using <see cref="DefaultFormatProvider"/>.
+        /// </summary>
+        /// <param name="formattable">The <see cref="FormattableString"/> to format.</param>
+        /// <returns>The formated string.</returns>
         public static string DokanFormat(FormattableString formattable)
             => formattable.ToString(DefaultFormatProvider);
 
+        /// <summary>
+        /// Prevent instantiation of the class
+        /// </summary>
         private FormatProviders()
         {
         }
 
+        /// <inheritdoc />
         public object GetFormat(Type service)
         {
             return service == typeof(ICustomFormatter) ? this : null;
         }
 
+        /// <inheritdoc />
         public string Format(string format, object arg, IFormatProvider provider)
         {
             if (arg == null) return NullStringRepresentation;
