879ab9c7d3b5e1df708584d1eac76bf84e46fc91
Adrien JUND <adrienj@google.com>
2020-09-28 15:26:20 +0200

    Mirror - Only set attributes if we just created the file
    * Always set attributes if we create the file
    * Remove normal attributes if another value is set

--- sample/DokanNetMirror/Mirror.cs
+++ sample/DokanNetMirror/Mirror.cs
@@ -196,9 +196,17 @@ namespace DokanNetMirror
                                        || mode == FileMode.Create))
                         result = DokanResult.AlreadyExists;
 
-                    if (mode == FileMode.CreateNew || mode == FileMode.Create) //Files are always created as Archive
-                        attributes |= FileAttributes.Archive;
-                    File.SetAttributes(filePath, attributes);
+                    bool fileCreated = mode == FileMode.CreateNew || mode == FileMode.Create || (!pathExists && mode == FileMode.OpenOrCreate);
+                    if (attributes != 0 && fileCreated)
+                    {
+                        FileAttributes new_attributes = attributes;
+                        new_attributes |= FileAttributes.Archive; // Files are always created as Archive
+                        // FILE_ATTRIBUTE_NORMAL is override if any other attribute is set.
+                        if (((new_attributes & FileAttributes.Normal) != 0) &&
+                            (new_attributes & (new_attributes - 1)) != 0)
+                            new_attributes &= ~FileAttributes.Normal;
+                        File.SetAttributes(filePath, new_attributes);
+                    }
                 }
                 catch (UnauthorizedAccessException) // don't have access rights
                 {
